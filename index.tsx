4536420c06807b0b3599b182afa95e0f50b3ca80
Rubén <r.pineda@ixiam.com>
2026-01-28 12:11:54 +0100

    Fix issue moving to subdomain to domain

--- index.tsx
+++ index.tsx
@@ -12,7 +12,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/auditoria_opacidad_boe/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
